5d0d21a44a6d8203c18a3f666ad011eb53923dc1
Abe Choe <hja.choe@gmail.com>
2022-11-10 18:02:08 -0500

    fixed local host

--- tests/App.spec.js
+++ tests/App.spec.js
@@ -1,6 +1,6 @@
 const { test, expect } = require("@playwright/test");
 
-const URL = "http://192.168.0.112:3000/";
+const URL = "http://localhost:3000";
 
 test("h1 text includes hello world", async ({ page }) => {
   await page.goto(URL);
